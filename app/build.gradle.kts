d16eb5a9db837cd8196df9046aeeb945b84bc60b
ahmmedrejowan <ahmmedrejowan@gmail.com>
2024-03-24 22:35:06 +0600

    Dependency to 0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.github.ahmmedrejowan:Licensy:38bff1bb81")
+    implementation("com.github.ahmmedrejowan:Licensy:0.1")
 
 }
